78d5cd4c715d1cb89e43b44d81ba8106bdfc8d0f
Lachlan.McPherson <lachlan.mcpherson@myob.com>
2025-01-21 09:56:50 +1100

    using standard class library

--- Hello-Word-Test/Hello-Word-Test/Program.cs
+++ Hello-Word-Test/Hello-Word-Test/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Hello_World_Standard;
+
+Helloer helloer = new Helloer();
+Console.WriteLine(helloer.SayHello());
---
+++ Hello-Word-Test/Hello-World-Standard/Helloer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World_Standard
+{
+    public class Helloer
+    {
+        public string SayHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
